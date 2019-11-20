5b52e909259123060158b7ce5adc7df6d34b4cbd
rohan <rohanvashisht29>
2019-11-21 02:04:24 +0530

    adding json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { BlogComponent } from './blog/blog.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { MessageComponent } from './message/message.component';
+import { MessageSecondComponent } from './message-second/message-second.component';
+import { RouterModule, Routes } from '@angular/router';
+
+// added for msg
+import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule,FormGroup,FormControl,Validators,FormBuilder} from '@angular/forms';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'message-one', component: MessageComponent },
+  { path: 'message-second', component: MessageSecondComponent }
+  
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    BlogComponent,
+    PortfolioComponent,
+    MessageComponent,
+    MessageSecondComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
+    // added for msg
+    FormsModule,
+    ReactiveFormsModule,
+
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false }
+    )
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/message-second/message-second.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-second',
+  templateUrl: './message-second.component.html',
+  styleUrls: ['./message-second.component.css']
+})
+export class MessageSecondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
