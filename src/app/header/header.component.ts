1b6808bebd5424e1dadeff31d1eb85105f38ff7d
rohan <rohanvashisht29>
2019-11-22 04:00:47 +0530

    added header with issue

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import  headerData from '../../assets/json/header.json';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  header
 
   constructor() { }
 
   ngOnInit() {
+    this.header=headerData.header;
+    console.log(this.header);
   }
 
 }
