223bae6d446be18c21df392933464502eed160d7
krishan Saini <krishan.saini@trantorinc.com>
2019-11-21 16:10:50 +0530

    added custom header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import headerData from '../../assets/json/components.json';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  header
 
   constructor() { }
 
   ngOnInit() {
+    this.header = headerData.header;
   }
 
 }
