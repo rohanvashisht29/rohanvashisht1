671dbfec78d48cd26e2dbf576177f1cdc9ca7dbb
rohan <rohanvashisht29>
2019-11-28 02:56:35 +0530

    footer changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import  footerData from '../../assets/json/footer.json';
 
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+
   footer
   constructor() { }
 
